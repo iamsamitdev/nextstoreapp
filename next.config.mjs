5033417fe89d933cac3a77d4e653bc34b6cd1c32
Samit Koyom <samitkoyom@gmail.com>
2024-05-04 23:18:31 +0700

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,38 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+  // output: 'export', // static export
+  // output: 'standalone', // serverless ssr
+  // distDir: 'out', // build folder
+
+  modularizeImports: {
+    "@mui/icons-material": {
+      transform: "@mui/icons-material/{{member}}",
+    },
+  },
+
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'itgenius.co.th, www.abc.co.th',
+        pathname: '**',
+      },
+      {
+        protocol: "http",
+        hostname: "*",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        pathname: "**",
+      },
+    ],
+    minimumCacheTTL: 0, // 0 second
+  }
+
+}
+
+export default nextConfig
